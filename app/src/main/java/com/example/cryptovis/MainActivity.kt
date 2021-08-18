bb9bd6ec31cb30be6441ee98ab793707f02cd0d8
MagnoVJ <magnovj@gmail.com>
2021-08-17 21:14:14 -0300

    8/17/2021 Commit 2

--- app/src/main/java/com/example/cryptovis/MainActivity.kt
+++ app/src/main/java/com/example/cryptovis/MainActivity.kt
@@ -2,10 +2,43 @@ package com.example.cryptovis
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var mainHandler: Handler
+    var numberDec = 1000
+
+    private val updateTextTask = object : Runnable {
+        override fun run() {
+            minusOneSecond()
+            mainHandler.postDelayed(this, 1000)
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        // Logic here
+
+        mainHandler = Handler(Looper.getMainLooper())
+    }
+
+     override fun onPause() {
+        super.onPause()
+        mainHandler.removeCallbacks(updateTextTask)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mainHandler.post(updateTextTask)
+    }
+
+    fun minusOneSecond() {
+        numberDec = numberDec - 1
+        val textView = findViewById(R.id.textViewNumDec) as TextView
+        textView.text = numberDec.toString()
     }
 }
